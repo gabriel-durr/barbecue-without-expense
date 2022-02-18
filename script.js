4fb04da923a28a1965bec0afbb965c2b7ae7bca0
Gabriel Durr <gabriel.durr@outlook.com>
2022-02-18 02:26:33 -0300

    refat placeholder and coments JS

--- script.js
+++ script.js
@@ -23,51 +23,54 @@ function calcular() {
     console.log("calculando ...");
     
 
-    //pegar a variável que pegou o id do input e em seguida, pegar o valor digitado nesse input com .value
+    // 3.1 - criar variáveis que recebem as variáveis que receberam o id dos inputs e em seguida .value pra pegar o valor digitado nos inputs 
 
     let adultos = inputAdultos.value; 
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value
 
-                        // 4 - Pegamos a função de Carne por pessoa e como parâmentro colocamos a duracao do evento, que pega o valor digitado no input, para adultos e criaças
+                        // 4 - Criar variável que recebe a função de Carne por pessoa e como parâmentro colocamos a duracao do evento, que pega o valor digitado no input, para adultos e criaças
     let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas);
 
 
-    // 6 - Pegamos a função de calculo da cerveja, em relação a duracao do evento como paramêtro  e removemos a parte da criança como na var anteior pois criança não bebe hehe 
+    // 6 - Criar variável que recebe a função de calculo da cerveja, em relação a duracao do evento como paramêtro  e removemos a parte da criança como na var anteior pois criança não bebe hehe 
 
 
     let qdtTotalCerveja = cervejaPP(duracao) * adultos;
 
 
-    // 8 - Pegamos a função de bebidas por pessoa e como parâmentro colocamos a duracao do evento, que pega o valor digitado no input, para adultos e criaças
+    // 8 - Criar variável que recebe a função de bebidas por pessoa e como parâmentro colocamos a duracao do evento, que pega o valor digitado no input, para adultos e criaças
 
     let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)/2 * criancas);
 
 
 
 
-    // 9 - pegamos o resultado e colocamos ele na tela com innerHTML, e atribuímos uma template string que pega a tag elemento p e dentro dela a variável que traz a a quantidade total de cada categoria.
+    // 9 - pegar a variável resultado que esta com a ID da DIV e colocamor ela na tela com innerHTML, e atribuímos uma template string que pega a tag elemento P e dentro dela as variáveis que traz a a quantidade total pra cada categoria.
     
   
 
+    // 10 -Dentro da template string:  Na quantidade total de carne colocamos a variável divida por 1000, para transformar o valor em grama, em kilograma.
+    
     resultado.innerHTML = `<p>${qdtTotalCarne / 1000}kg de Carne</p>`
 
-    // 10 -Dentro da template string:  Na quantidade total de carne colocamos a variável divida por 1000, para transformar o valor em grama, em kilograma.
     
 
 
-    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Lata(s) de Cerveja</p>`
-
     // 11 - Dentro da template string:  Na quantidade total de cerveja colocamos a variável divida por 355, para transformar o valor em ml de cerveja, para unidades de lata. E para não ficar um valor quebrado, utilizamos o método Math.ceil, para transformar em número inteiro do teto, e arredondar o valor. 
 
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Lata(s) de Cerveja</p>`
+
+
+
+    // 12 - Dentro da template string:  Na quantidade total de cerveja colocamos a variável divida por 2000, para transformar o valor em ml de Bebidas(Refri), para unidades de garrafas de 2l. E para não ficar um valor quebrado, utilizamos o método Math.ceil, para transformar em número inteiro do teto, e arredondar o valor. 
     
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Garrafa(s) de Refrigerante</p>`
 
     
-    // 12 - Dentro da template string:  Na quantidade total de cerveja colocamos a variável divida por 2000, para transformar o valor em ml de bebidas, para unidades de garrafas. E para não ficar um valor quebrado, utilizamos o método Math.ceil, para transformar em número inteiro do teto, e arredondar o valor. 
 
 
-    //  - Após a primneira linha colocamos o += para não subistuir a linha anterior, que significa que estamos recebendo o anterior + o que estamos acrescentando.
+    //  - Após a primneira linha colocamos o += para não subistuir a linha anterior no innterHTML, que significa que estamos recebendo o anterior + o que estamos acrescentando.
 
     console.log(qdtTotalCarne);
 
@@ -78,7 +81,7 @@ function calcular() {
 
 
 
-// 3 -  Função para multiplicar a quantidade de carne por pessoa, com base no tempo do evento, se passar de 6 horas o valor é aumenta e então é criada uma condicional para isso.
+// 3.2 - Criar Função para multiplicar a quantidade de carne por pessoa, com base no tempo do evento, se passar de 6 horas o valor é aumentado e então é criada uma condicional para isso.
 
 function carnePP(duracao){
 
